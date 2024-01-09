51a80cc60f41efeb47f7d6c29155f335ccb99197
mathias <mathiasdurat@gmail.com>
2024-01-09 09:28:08 +0100

    correcting user get for images

--- app/Controllers/Http/UserController.ts
+++ app/Controllers/Http/UserController.ts
@@ -1,14 +1,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
+import Drive from "@ioc:Adonis/Core/Drive";
 
 export default class UserController {
   public async index(HttpContextContract: HttpContextContract) {
-    let users = {}
     if (HttpContextContract.auth.user) {
-      users = await User.query()
+      let users = await User.query()
         .select('id', 'username', 'avatar_url')
         .whereNot('id', HttpContextContract.auth.user.id)
+      for (let user of users) {
+        user.avatarUrl = await Drive.getSignedUrl(user.avatarUrl)
+      }
+      HttpContextContract.response.send(JSON.stringify(users))
     }
-    HttpContextContract.response.send(JSON.stringify(users))
+    HttpContextContract.response.unauthorized()
   }
 }
